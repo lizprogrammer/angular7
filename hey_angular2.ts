3aa6524b8fd17e758c42f7b32ae4962850ba496d
Elizabeth Rosso <lizprogrammer@yahoo.com>
2019-02-27 17:53:09 -0500

    angular

---
+++ hey_angular2.ts
@@ -0,0 +1,8 @@
+
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+let alternativeWay = (message as string).endsWith('c');
+
+
+
---
+++ main.ts
@@ -0,0 +1,4 @@
+
+
+console.log("whassup dog");
+
